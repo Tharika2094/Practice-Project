2c4354b0cea54ca4af4628ec483b068032b04d1c
Tharika2094 <tvijayangi@gmail.com>
2025-05-27 07:48:30 +0100

    Hero Text with animation

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,7 +5,13 @@ function Home() {
   return (
     <div className='homeBody'>
       <div className='heroSection'>
-        
+        <div className='hero-text'>
+          <span className='welcomeText'>Welcome to Verda Studio</span>
+          <div className='hero-text-div'>
+            <span className='hero-text1'>Architectural, Interior, Landscape Design & Build In a One Stop Solution</span>
+            
+          </div>
+        </div>
 
       </div>
     </div>
